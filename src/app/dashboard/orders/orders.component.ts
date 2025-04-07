ec687e7239608155df0fc4d637fca87ace7607f7
Muhammad Asif <masif4732714@gmail.com>
2025-04-07 18:43:00 +0500

    Singup form added deleted .spec files

--- src/app/dashboard/orders/orders.component.ts
+++ src/app/dashboard/orders/orders.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-orders',
@@ -6,6 +6,11 @@ import { Component } from '@angular/core';
   templateUrl: './orders.component.html',
   styleUrl: './orders.component.css'
 })
-export class OrdersComponent {
+export class OrdersComponent implements OnInit{
+  ngOnInit(): void {
+    console.log('Orders component initialized');
+    
+  }
+
 
 }
